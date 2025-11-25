678a2be70ed6c138f028df43f190151bd967858c
sunday8523 <sunday55140@gmail.com>
2025-11-26 01:10:07 +0700

    fix vac

--- src/components/med_data/med_data.jsx
+++ src/components/med_data/med_data.jsx
@@ -7,6 +7,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { motion, AnimatePresence } from "framer-motion";
 import { ChevronDown, PlusCircle, ClipboardList } from "lucide-react";
+import VaccinePopup from '../VaccinePopup.jsx';
 
 const MedData = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
